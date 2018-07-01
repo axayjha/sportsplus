0510621c56188ba72def2d8975decbfa2eba62d8
Akshay <akshayjha@live.in>
2018-07-02 02:21:48 +0530

    updated controller and repo

---
+++ src/main/java/com/cse/sportsplus/controller/ScheduleController.java
@@ -0,0 +1,39 @@
+package com.cse.sportsplus.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cse.sportsplus.models.Schedule;
+import com.cse.sportsplus.repository.ScheduleRepository;
+
+@RestController
+@RequestMapping("/schedule") 
+public class ScheduleController {
+	
+	@Autowired
+	private ScheduleRepository scheduleRepository;
+	
+	
+	@PostMapping("/add")
+	public Schedule addSchedule(@RequestBody Schedule schedule) {
+		Schedule persistedSchedule = scheduleRepository.save(schedule); 
+		// line above saves the object in database
+		// and returns same schedule object with id value updated as in DB  
+		return persistedSchedule;
+	}
+	
+	@GetMapping("/getAll")
+	public List<Schedule> getAll() {
+		List <Schedule> list_of_schedules = scheduleRepository.findAll();
+		return list_of_schedules;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/cse/sportsplus/repository/ScheduleRepository.java
@@ -0,0 +1,12 @@
+package com.cse.sportsplus.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cse.sportsplus.models.Schedule;
+
+public interface ScheduleRepository  extends JpaRepository<Schedule, Long> {
+
+}
+
+
+ 
