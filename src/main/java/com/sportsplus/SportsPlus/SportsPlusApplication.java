783291ab31cd540b4f517bf9720c2875ab4c9671
Akshay <akshayjha@live.in>
2018-07-02 00:05:23 +0530

    initial commit

---
+++ src/main/java/com/sportsplus/SportsPlus/SportsPlusApplication.java
@@ -0,0 +1,12 @@
+package com.sportsplus.SportsPlus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsPlusApplication.class, args);
+	}
+}
