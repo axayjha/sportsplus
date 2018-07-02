7d01af37940b59a610384567ade8a01db5ca8a7a
Akshay <akshayjha@live.in>
2018-07-02 11:06:43 +0530

    updated config

--- src/main/java/com/cse/sportsplus/config/SportsPlusApplication.java
+++ src/main/java/com/cse/sportsplus/config/SportsPlusApplication.java
@@ -2,8 +2,15 @@ package com.cse.sportsplus.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan("com.cse") // search for all the beans in the packages
+@EnableJpaRepositories(basePackages = "com.cse") // enables the jpa repo for the base package
+@EntityScan("com.cse")  // looks for all the objects that have @Entity above it
+
 public class SportsPlusApplication {
 
 	public static void main(String[] args) {
