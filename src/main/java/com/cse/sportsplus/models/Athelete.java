08d92e6e522b42bd099bfa34f8a5effdde1a2896
rishabhshairy <rishusinha8@gmail.com>
2018-07-02 22:19:03 +0530

    Changes in athlete  data model

--- src/main/java/com/cse/sportsplus/models/Athelete.java
+++ src/main/java/com/cse/sportsplus/models/Athelete.java
@@ -4,53 +4,99 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
+@Entity
+@Table(name="athlete_tbl")
 public class Athelete implements Serializable{
+	@Id
+	@GeneratedValue
+	@Column(name="id")
     private long id;
+	@NotNull
+	@Column(name="firstName")
     private String firstName;
+	@NotNull
+	@Column(name="lastName")
     private String lastName;
+	@NotNull
+	@Column(name="DOB")
     private String  dob;
+	@Column(name="Age")
     private long age;
+	@NotNull
+	@Column(name="Gender")
     private char gender;
+	@NotNull
+	@Column(name="Contact No")
     private long contactNo;
+	@Column(name="Address")
     private String address;
+	@NotNull
+	@Column(name="city")
+	private String city;
+	@NotNull
+	@Column(name="pincode")
+	private long pincode;
+	@Column(name="ParentFullName")
     private String parentFullName;
+	@Column(name="ParentAddress")
     private String parentAddress;
+	@NotNull
+	@Column(name="ParentContact")
     private long parentContact;
-    //primary sports check for implementation...
+	@NotNull
+	@Column(name="PrimarySport")
+    private String primarySport;
+	@Column(name="TrainigBase")
     private String trainingBase;
+	@Column(name="College")
     private String college;
-    private ArrayList<Integer> groupId;
-    private long coachId;
+    //private ArrayList<Integer> groupId;
+    
+	private long coachId;
     private String level;
     private String residentStatus;
     private ArrayList<String> athleteStatus;
     private String bloodGroup;
 
-    public Athelete(long id, String firstName, String lastName, String dob, long age, char gender, long contactNo, String address, String parentFullName, String parentAddress, long parentContact, String trainingBase, String college, ArrayList<Integer> groupId,
-                    long coachId, String level, String residentStatus, ArrayList<String> athleteStatus, String bloodGroup) {
-        this.id = id;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dob = dob;
-        this.age = age;
-        this.gender = gender;
-        this.contactNo = contactNo;
-        this.address = address;
-        this.parentFullName = parentFullName;
-        this.parentAddress = parentAddress;
-        this.parentContact = parentContact;
-        this.trainingBase = trainingBase;
-        this.college = college;
-        this.groupId = groupId;
-        this.coachId = coachId;
-        this.level = level;
-        this.residentStatus = residentStatus;
-        this.athleteStatus = athleteStatus;
-        this.bloodGroup = bloodGroup;
-    }
+    
 
-    public Athelete() {
+    public Athelete(long id, @NotNull String firstName, @NotNull String lastName, @NotNull String dob, long age,
+			@NotNull char gender, @NotNull long contactNo, String address, @NotNull String city, @NotNull long pincode,
+			String parentFullName, String parentAddress, @NotNull long parentContact, @NotNull String primarySport,
+			String trainingBase, String college, long coachId, String level, String residentStatus,
+			ArrayList<String> athleteStatus, String bloodGroup) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.dob = dob;
+		this.age = age;
+		this.gender = gender;
+		this.contactNo = contactNo;
+		this.address = address;
+		this.city = city;
+		this.pincode = pincode;
+		this.parentFullName = parentFullName;
+		this.parentAddress = parentAddress;
+		this.parentContact = parentContact;
+		this.primarySport = primarySport;
+		this.trainingBase = trainingBase;
+		this.college = college;
+		this.coachId = coachId;
+		this.level = level;
+		this.residentStatus = residentStatus;
+		this.athleteStatus = athleteStatus;
+		this.bloodGroup = bloodGroup;
+	}
+
+	public Athelete() {
     }
 
     public long getId() {
@@ -157,13 +203,13 @@ public class Athelete implements Serializable{
         this.college = college;
     }
 
-    public ArrayList<Integer> getGroupId() {
-        return groupId;
-    }
-
-    public void setGroupId(ArrayList<Integer> groupId) {
-        this.groupId = groupId;
-    }
+//    public ArrayList<Integer> getGroupId() {
+//        return groupId;
+//    }
+//
+//    public void setGroupId(ArrayList<Integer> groupId) {
+//        this.groupId = groupId;
+//    }
 
     public long getCoachId() {
         return coachId;
@@ -204,4 +250,20 @@ public class Athelete implements Serializable{
     public void setBloodGroup(String bloodGroup) {
         this.bloodGroup = bloodGroup;
     }
+    
+    public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public long getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(long pincode) {
+		this.pincode = pincode;
+	}
 }
