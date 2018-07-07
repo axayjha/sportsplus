d0fb3aabc7e7d0a25c5368a519dd71da60eb6492
Rishabh Shairy <rishusinha8@gmail.com>
2018-07-08 00:56:04 +0530

    Delete CoachRepository.java

--- src/main/java/com/cse/sportsplus/repository/CoachRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cse.sportsplus.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cse.sportsplus.models.Coach;
-
-public interface CoachRepository extends JpaRepository<Coach, Long> {
-
-}
