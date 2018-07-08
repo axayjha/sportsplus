696b3d126cfe9248ae4dae054a1cea131c3d09b9
Arnab Sen <arnb.sen@hotmail.com>
2018-07-08 15:53:12 +0530

    Fix merge.

--- src/main/java/com/cse/sportsplus/repository/CoachRepository.java
+++ src/main/java/com/cse/sportsplus/repository/CoachRepository.java
@@ -6,8 +6,4 @@ import com.cse.sportsplus.models.Coach;
 
 public interface CoachRepository extends JpaRepository<Coach, Long> {
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> ef259aa64e698cbd4fc5043b1d42e79ac93c2e66
--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -193,3 +193,135 @@ app.controller("groupCtrl", function($scope, $http) {
 		});
 	};
 });
+app.controller("scheduleCtrl", function($scope, $http) {
+	
+
+	$scope.init =function(){
+		console.log("Init");
+		var date = new Date();
+		$scope.currentDate = formatDate(date);
+		console.log($scope.currentDate);
+		$scope.range4=[];
+		for (var i = 1; i <= 12; i++) {
+			$scope.range4.push(i);
+		}
+		console.log($scope.range4);
+		getAll($http, $scope);
+		$scope.editAllowed = true;
+	};
+
+	//Creating validations and functions
+	
+	//Validation if the fields are empty or not
+	var addScheduleValidate = function(scheduleObject){
+		Object.keys(scheduleObject).forEach(function(key){
+			if(scheduleObject[key] == ""){
+				return false;
+			}
+			return true;
+		});
+	};
+
+	
+	//Linking to HTML
+	//Add Schedule
+	$scope.addSchedule = function(newSchedule,day,month,year){
+		var checkIfOkay = addScheduleValidate(newSchedule);
+		//Check if selected date is greater than current date date
+		var date = new Date();
+		var s = day + "-" + month + "-" + year;
+		var inputDate = new Date(s);
+		if(inputDate < date){
+			$scope.dateMessage = "Selected Date should be greater than current date";
+			checkIfOkay = false;
+		}
+		$scope.newSchedule.date_time = inputDate.getMilliseconds();
+		if(checkIfOkay){
+			$http({
+				method : 'POST',
+				url : 'http://localhost:8080/schedule/add',
+				headers: { 'Content-Type': 'application/json' },
+				data:newSchedule
+			}).success(function(data, status) {
+				console.log(data);
+				$scope.message = "Sucessfully Added. You are good to go";
+			}).error(function(data, status) {
+				$scope.status = status;
+				$scope.data = "Request failed";
+			});
+		}else{
+			$scope.message = "Fields were left empty. Please fill in the requires Details";
+		}
+	};
+	//Update Schedule
+	$scope.updateSchedule = function(Schedule) {
+		$scope.addSchedule(Schedule);
+	};
+
+
+	//Delete Schedule
+	$scope.deleteSchedule = function(Schedule){
+		$http({
+			method : 'POST',
+			url : 'http://localhost:8080/schedule/delete',
+			headers: { 'Content-Type': 'application/json' },
+			data:Schedule
+		}).success(function(data, status) {
+			console.log(data);
+			$scope.deleteMessage = "Sucessfully Deleted";
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.deleteMessage = "Request failed";
+		});
+	};
+
+	//Get All Schedules
+	var getAll = function($http, $scope){
+		$http({
+			method : 'GET',
+			url : 'http://localhost:8080/schedule/getAll',
+			headers: { 'Content-Type': 'application/json' },
+		}).success(function(data, status) {
+			$scope.allSchedules = data;
+			console.log($scope.allSchedules);
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.message = "Request failed";
+		});
+	}
+
+	//Additional Methods for processing
+	$scope.fetchAllAcademyID = function(){
+		$http({
+			method : 'GET',
+			url : 'http://localhost:8080/schedule/getAcademyID',
+			headers: { 'Content-Type': 'application/json' },
+		}).success(function(data, status) {
+			
+			$scope.academyID = data;
+			console.log($scope.academyID);
+			$scope.enableGroup = true;
+		}).error(function(data, status) {
+			$scope.status = status;
+			$scope.deleteMessage = "Request failed";
+		});
+	};
+	$scope.formatDate1 = function(milli){
+		var fdate = new Date(milli);
+		$scope.day = fdate.getDate();
+		$scope.month = fdate.getMonth()+1;
+		$scope.year = fdate.getFullYear();
+	};
+});
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('-');
+}
