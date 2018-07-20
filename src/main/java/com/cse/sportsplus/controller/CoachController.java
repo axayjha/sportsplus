7b86c4cab54c00c386e226d1079c7065c0c20970
ananyadas15 <40362283+ananyadas15@users.noreply.github.com>
2018-07-20 22:09:32 +0530

    Update CoachController.java

--- src/main/java/com/cse/sportsplus/controller/CoachController.java
+++ src/main/java/com/cse/sportsplus/controller/CoachController.java
@@ -16,29 +16,33 @@ import com.cse.sportsplus.repository.CoachRepository;
 
 
 @RestController
-@RequestMapping("/coach")
+@RequestMapping("/coach") //Class level mapping
 public class CoachController {
 	
-	@Autowired
+	@Autowired //implements IOC
 	private CoachRepository coachRepository;
 	
+	//Add new Coach
 	@PostMapping("/create")
 	public Coach coachCreate(@RequestBody Coach coach) {
 		Coach coh = coachRepository.save(coach);
 		return coh;
 	}
 	
+	//Display details of existing coaches
 	@GetMapping("/all")
 	public List<Coach> coachList() {
 		List<Coach> coaches = coachRepository.findAll();
 		return coaches;
 	}
 	
+	//Delete specific coach data
 	@PostMapping("/delete")
 	public void coachDelete(@RequestBody Coach cid) {
 		coachRepository.deleteById(cid.getCoach_id());
 	}
 	
+	//Update existing coach record
 	@PostMapping("/update")
 	public Coach coachUpdate(@RequestBody Coach coach) {
 		Coach coh = coachRepository.save(coach); // note that given coach_id must be present
