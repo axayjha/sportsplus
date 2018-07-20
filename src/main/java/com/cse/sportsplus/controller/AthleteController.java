c3965c6922c814bc04b8bbb65731d1edc64d23bc
ananyadas15 <40362283+ananyadas15@users.noreply.github.com>
2018-07-20 22:07:08 +0530

    Update AthleteController.java

--- src/main/java/com/cse/sportsplus/controller/AthleteController.java
+++ src/main/java/com/cse/sportsplus/controller/AthleteController.java
@@ -11,16 +11,16 @@ import com.cse.sportsplus.repository.AthleteRepository;
 import com.cse.sportsplus.repository.CoachRepository;
 
 @RestController
-@RequestMapping("/Athlete")
+@RequestMapping("/Athlete") //class level mapping
 public class AthleteController {
 	
-	@Autowired
+	@Autowired //implements IOC
 	private AthleteRepository arepo;
 	
 	@Autowired
 	private CoachRepository coachRepo;
 	
-	
+	//Display data of existing athletes
 	@GetMapping("/getallAthlete")
 	public List<Athlete> AthleteList()
 	{
@@ -30,7 +30,7 @@ public class AthleteController {
 	
 	}
 	
-
+	//Add new athlete data
 	@PostMapping("/addAthlete")
 	public Athlete AthleteAdd(@RequestBody Athlete Athlete)
 	{
@@ -39,27 +39,36 @@ public class AthleteController {
 		
 		return Athlete;
 	}
+	
+	//Delete specific athlete
 	@PostMapping("/deleteAthlete")
 	public Athlete AthleteDelete(@RequestBody Athlete athlete)
 	{
 		arepo.deleteById(athlete.getId());
 		return athlete;
 	}
+	
+	//Update records of existing athlete
 	@PostMapping("/AthleteUpdate")
 	 public Athlete AthleteUpdate(@RequestBody Athlete Athlete)
 	 {
 		 return Athlete;
 		 
 	 }
+	
+	//Get details of athlete by his/her name
 	 @GetMapping("/AthleteByName/{name}")
     public Athlete getAthleteByName(@PathVariable(value ="name" ) String name){
 	    return arepo.findByFirstName(name);
      }
 	 
+	//Display Coach by Group ID
 	 @GetMapping("/coachByGroupID")
 	 public List<java.math.BigInteger> getCoachID(@RequestParam Long group_id){
 		 return coachRepo.getAllCoachIDByGroupID(group_id);
 	 }
+	
+	//Display athlete by athlete ID
 	 @GetMapping("/athleteByID")
 	 public Athlete getByID(@RequestParam Long id) {
 		 return arepo.findAth(id);
